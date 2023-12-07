3c1694f0d7e9e62b64ea50fae6c4bf34ff66c1d2
Alexander Chehovskii <let.value@gmail.com>
2023-12-08 01:07:36 +0600

    ci

--- src/Program.cs
+++ src/Program.cs
@@ -40,58 +40,72 @@ if (manifest is null || manifest.ManifestType != "minecraftModpack")
 
 Console.WriteLine($"Modpack: {manifest.Name}, Version:{manifest.Version}");
 Console.WriteLine($"Minecraft version: {manifest.Minecraft.Version}");
-Console.WriteLine($"Mod loaders: {string.Join(", ", manifest.Minecraft.ModLoaders.Select(x => $"{(x.Primary ? "primary" : "")} {x.Id}"))}");
+var modLoaders = string.Join(", ", manifest.Minecraft.ModLoaders.Select(x => $"{(x.Primary ? "primary" : "")} {x.Id}"));
+Console.WriteLine($"Mod loaders: {modLoaders}");
 
 var curseForgeClient = new CurseForgeClient(apiToken);
 var httpClient = new HttpClient();
 
-foreach (var file in manifest.Files.Take(1))
-{
-    var uri = await curseForgeClient.GetDownloadUri(file);
-    var fileName = Path.GetFileName(uri?.LocalPath);
+var modList = new List<string>();
 
-    if (fileName is null)
+await Parallel.ForEachAsync(
+    manifest.Files,
+    new ParallelOptions { MaxDegreeOfParallelism = 4 },
+    async (file, cancellationToken) =>
     {
-        Console.WriteLine("Failed to get download url.");
-        return;
+        var uri = await curseForgeClient.GetDownloadUri(file);
+        var fileName = Path.GetFileName(uri?.LocalPath);
+
+        if (fileName is null)
+        {
+            Console.WriteLine("Failed to get download url.");
+            return;
+        }
+
+        modList.Add(fileName);
+
+        var filePath = Path.Combine(outputPath, "mods", fileName);
+
+        if (File.Exists(filePath))
+        {
+            Console.WriteLine($"File {fileName} already exists.");
+            return;
+        }
+
+        var directoryPath = Path.GetDirectoryName(filePath);
+
+        if (directoryPath is null)
+        {
+            Console.WriteLine($"Failed to get directory path for {fileName}");
+            return;
+        }
+
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        Console.WriteLine($"Downloading {fileName}...");
+
+        using var response = await httpClient.GetAsync(uri, cancellationToken);
+        await using var stream = await response.Content.ReadAsStreamAsync();
+        await using var fileStream = File.Create(filePath);
+        await stream.CopyToAsync(fileStream, cancellationToken);
+
+        Console.WriteLine($"Downloaded {fileName}.");
     }
-
-    var filePath = Path.Combine(outputPath, "mods", fileName);
-
-    if (File.Exists(filePath))
-    {
-        Console.WriteLine($"File {fileName} already exists.");
-        continue;
-    }
-
-    var directoryPath = Path.GetDirectoryName(filePath);
-
-    if (directoryPath is null)
-    {
-        Console.WriteLine($"Failed to get directory path for {fileName}");
-        continue;
-    }
-
-    if (!Directory.Exists(directoryPath))
-    {
-        Directory.CreateDirectory(directoryPath);
-    }
-
-    Console.WriteLine($"Downloading {fileName}...");
-
-    using var response = await httpClient.GetAsync(uri);
-    await using var stream = await response.Content.ReadAsStreamAsync();
-    await using var fileStream = File.Create(filePath);
-    await stream.CopyToAsync(fileStream);
-
-    Console.WriteLine($"Downloaded {fileName}.");
-}
+);
 
 var overrideEntries = archive.Entries.Where(x => x.FullName.StartsWith(manifest.Overrides)).ToList();
 foreach (var entry in overrideEntries)
 {
     var relativePath = entry.FullName.Replace($"{manifest.Overrides}/", "");
     var filePath = Path.Combine(outputPath, relativePath);
+
+    if (relativePath.StartsWith("mods/"))
+    {
+        modList.Add(Path.GetFileName(filePath));
+    }
 
     if (File.Exists(filePath))
     {
@@ -113,3 +127,19 @@ foreach (var entry in overrideEntries)
 
     entry.ExtractToFile(filePath);
 }
+
+var description = $@"
+# Modpack
+
+- Name: {manifest.Name},
+- Version: {manifest.Version}
+- Minecraft version: {manifest.Minecraft.Version}
+- Mod loaders: {modLoaders}
+
+## Mods ({modList.Count})
+
+{string.Join(Environment.NewLine, modList.Select(x => $"- {x}"))}
+";
+
+var readmePath = Path.Combine(outputPath, "README.md");
+await File.WriteAllTextAsync(readmePath, description);
